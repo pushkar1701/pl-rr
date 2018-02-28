2140864e05a69fa260de7a6a5727c0f8981c6a8e
Pushkar Kathuria <c-pushkar.kathuria@charter.com>
2018-03-01 00:39:23 +0530

    Redux connectivity- store, actions, reducers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import 'babel-polyfill';
 import React, {PropTypes} from 'react';
 import {render} from 'react-dom';
+import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
+import configureStore from './store/configureStore';
 import './assets/styles/styles.scss';
 
+const store = configureStore();
 render (
-    <Router history={browserHistory} routes={routes} />,
-    document.getElementById('resume-wrapper')
-)
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
+        document.getElementById('resume-wrapper')
+);
